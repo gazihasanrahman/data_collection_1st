0e06d8f0b2f6b0e370a92758be5c91bb3803f140
Gazi Hasan Rahman <gazi.hasan.rahman@gmail.com>
2025-07-03 14:54:28 +0100

    Refactor API endpoint to handle multiple paths and improve error handling

--- listener.py
+++ listener.py
@@ -7,6 +7,7 @@ from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 import uvicorn
 import config
 from utils.logger import logger_1st
+from functools import wraps
 
 
 # configs
@@ -22,24 +23,21 @@ def verify_api_key(credentials: HTTPAuthorizationCredentials = Depends(security)
     return True
 
 
-@app.post("/first_data")
-async def receive_data(data: Dict[str, Any], api_key_verified: bool = Depends(verify_api_key)):
+@app.post("/{path:path}")
+async def receive_data(path: str, data: Dict[str, Any], api_key_verified: bool = Depends(verify_api_key)):
     try:
-        sqs_client.send_message(QueueUrl=config.SQS_QUEUE_URL, MessageBody=json.dumps(data))
-        return {"status": "success"}
+        match path:
+            case "1st-data":
+                sqs_client.send_message(QueueUrl=config.SQS_QUEUE_URL, MessageBody=json.dumps(data))
+                return {"status": "success"}
+            case "health":
+                return {"status": "healthy"}
+            case _:
+                raise HTTPException(status_code=404, detail="Invalid path")
+
     except Exception as e:
         logger_1st.error(f"Error in received data: {e}")
         raise HTTPException(status_code=500, detail="Internal server error")
-
-
-@app.get("/")
-async def root():
-    return {"message": "The API server is running"}
-
-
-@app.get("/health")
-async def health_check():
-    return {"status": "healthy"}
 
 
 if __name__ == "__main__":
